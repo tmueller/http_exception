6b277e792052a6ac1abc4b6bcb950df674f637c2
Thomas Mueller <tmueller@cpan.org>
2012-08-17 00:37:33 +0200

    deleted 2 unused testclasses

--- t/lib/Test/HTTP/Exception/UserOne.pm
+++
@@ -1,8 +0,0 @@
-package Test::HTTP::Exception::UserOne;
-
-use strict;
-use warnings;
-
-use HTTP::Exception;
-
-1;
--- t/lib/Test/HTTP/Exception/UserTwo.pm
+++
@@ -1,8 +0,0 @@
-package Test::HTTP::Exception::UserTwo;
-
-use strict;
-use warnings;
-
-use HTTP::Exception;
-
-1;
