74eed16bfaf36ced823399c3c68d4df658de8658
Tim Bunce <tim@tigerlms.com>
2015-05-25 13:56:57 +0000

    Use q{} for _status_message so message can contain a single quote
    Fixes #3

--- lib/HTTP/Exception/Loader.pm
+++ lib/HTTP/Exception/Loader.pm
@@ -56,7 +56,7 @@ sub _make_exceptions {
 
             package $package_name_code;
             sub code            () { $statuscode }
-            sub _status_message () { '$http_status_message' }
+            sub _status_message () { q{$http_status_message} }
 
             package $package_name_message;
             use Scalar::Util qw(blessed);
